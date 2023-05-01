441906ac2c5ebc8c0292d84285e7c6eef2f7c76d
Eyal Harel <eyalh747@gmail.com>
2023-05-01 17:55:52 +0300

    Update Tableau Portfolio Project SQL Queries.sql

--- Tableau Portfolio Project SQL Queries.sql
+++ Tableau Portfolio Project SQL Queries.sql
@@ -1,7 +1,6 @@
 /*
-Queries used for Tableau Project
+Covid SQL queries used for Tableau Project
 */
-
 
 
 -- 1. 
@@ -12,17 +11,6 @@ From Portfolio_Project..CovidDeaths$
 where continent is not null 
 --Group By date
 order by 1,2
-
--- Just a double check based off the data provided
--- numbers are extremely close so we will keep them - The Second includes "International"  Location
-
-
---Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
---From PortfolioProject..CovidDeaths
-----Where location like '%states%'
---where location = 'World'
-----Group By date
---order by 1,2
 
 
 -- 2. 
