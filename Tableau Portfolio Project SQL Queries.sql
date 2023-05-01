36e3d772a896246868b843fd40cebe20452b358e
Eyal Harel <eyalh747@gmail.com>
2023-05-01 18:00:39 +0300

    Update Tableau Portfolio Project SQL Queries.sql

--- Tableau Portfolio Project SQL Queries.sql
+++ Tableau Portfolio Project SQL Queries.sql
@@ -7,20 +7,14 @@ Covid SQL queries used for Tableau Project
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From Portfolio_Project..CovidDeaths$
---Where location like '%states%'
 where continent is not null 
---Group By date
 order by 1,2
 
 
 -- 2. 
 
--- We take these out as they are not inluded in the above queries and want to stay consistent
--- European Union is part of Europe
-
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
 From Portfolio_Project..CovidDeaths$
---Where location like '%states%'
 Where continent is null 
 and location not in ('World', 'European Union', 'International')
 Group by location
